aaac75bde32326c9da36c3456848f6ee218c5f0f
mugishap <precieuxmugisha@gmail.com>
2022-06-29 14:16:10 +0200

    modifying user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
 from django.db import models
+from traitlets import default
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = 'pass'
+    user_id = 'pass'
+    bio = 'pass'
+    profileimg = models.ImageField(upload_to = 'profile_images',default='profile_picture.png')
+    location = 'pass'
