f585034b227f3ab2adb80e8267f44a96a5dd9712
mugishap <precieuxmugisha@gmail.com>
2022-06-29 20:59:59 +0200

    modified models to avoid colliding with default django models

--- core/models.py
+++ core/models.py
@@ -7,11 +7,16 @@ User = get_user_model()
 
 
 class Profile(models.Model):
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
+
+    userid = models.IntegerField()
+
     bio = models.TextField(blank=True)
+
     profileimg = models.ImageField(
         upload_to='profile_images', default='profile_picture.png')
+
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
