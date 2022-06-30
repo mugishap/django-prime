e187a54a5bf17d4187c682838c5b8fd6cbf1ee09
mugishap <precieuxmugisha@gmail.com>
2022-06-30 17:06:56 +0200

    adding user and profiles

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,7 +9,29 @@ def index(request):
     return render(request,'index.html')
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        email = request.POST['email']
+
+        if password == cpassword:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username already exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+
+                # se in
+        else:
+            messages.info(request,'Passwords do not match')
+            return redirect('signup')
+    else:
+        return render(request,'signup.html')
 
     # def profile(request):
 #     return render(request,'profile.html')
