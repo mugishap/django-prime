dc6acf517d5ab96bc23970a93513e3c870945cf0
mugishap <precieuxmugisha@gmail.com>
2022-07-07 14:55:12 +0200

    added posting feature

--- core/views.py
+++ core/views.py
@@ -6,14 +6,18 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from matplotlib import image
 from requests import post
-from .models import Profile
+from .models import Profile, Post
 
 # Create your views here.
 
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile':user_profile, 'posts':posts})
 
 
 def signup(request):
@@ -106,11 +110,22 @@ def settings(request):
             user_profile.location = location
             user_profile.save()
 
-
         return redirect('settings')
 
     return render(request, 'setting.html', {'user_profile': user_profile})
 
+
 @login_required
 def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image=request.FILES.get('image_upload')
+        caption=request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+        return redirect('/')
+
+    else:
+        return redirect('/')
     return HttpResponse('<h1>Upload View</h1>')
